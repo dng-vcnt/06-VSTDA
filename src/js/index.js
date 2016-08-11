4b19c2ea5c24d2848dfb7bbf3c8495980c845949
Vincent Duong <vincentduong@Vincents-MacBook-Pro.local>
2016-08-11 15:38:38 -0700

    Add ng-sortable feature and button function

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
-var app = angular.module('vstda', []);
+var app = angular.module('vstda', ['as.sortable']);
 
 app.controller('vstdaController', function($scope) {
-	$scope.todoList = [];
+	$scope.todoList = [
+		{"description": "Clean car" , "priority": "medium", "order": 2},
+		{"description": "Clean self" , "priority": "high", "order": 1},
+		{"description": "Clean poop" , "priority": "low", "order": 3},
+		{"description": "Program" , "priority": "low", "order": 3},
+		{"description": "Watch cartoons" , "priority": "high", "order": 1}
+	];
 	$scope.propertyOrder = null;
 	$scope.reverse = false;
 
@@ -12,23 +18,35 @@ app.controller('vstdaController', function($scope) {
 		var order;
 		if ($scope.todoItem.priority == "high") {
 			order = 1;
-		}
-
-		if ($scope.todoItem.priority == "medium") {
+		} 
+		else if ($scope.todoItem.priority == "medium") {
 			order = 2;
-		}
-
-		if ($scope.todoItem.priority == "low") {
+		} 
+		else if ($scope.todoItem.priority == "low") {
 			order = 3;
+		}
+		else {
+			order = 2;
+			$scope.todoItem.priority = "medium";
 		}
 
 		// Push to array
 		$scope.todoList.push({"description": $scope.todoItem.description, 
 			"priority": $scope.todoItem.priority,
 			"order": order});
-
-		console.log($scope.todoList);
-
 	}
+
+	$scope.delete = function(index) {
+		$scope.todoList.splice(index, 1);
+	}
+
+	$scope.dragControlListeners = {
+    // accept: function (sourceItemHandleScope, destSortableScope) {return boolean}//override to determine drag is allowed or not. default is true.
+    // itemMoved: function (event) { },//Do what you want
+    // orderChanged: function(event) { },//Do what you want
+    // containment: '#board'//optional param.
+    // clone: true //optional param for clone feature.
+    // allowDuplicates: false //optional param allows duplicates to be dropped.
+  	};
 
 });
